85aa075805708f514d0abfe27514b543f097e1bf
Markus Dieckmann <markus.dieckmann@edataprocessing.de>
2018-07-24 15:23:11 +0200

    Continue With

---
+++ TaskExamples/ContinuationTask/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContinuationTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<int> t = Task.Run(() =>
+            {
+                return 42;
+            }).ContinueWith((i) => { return i.Result * 2; });
+            Console.WriteLine(t.Result);
+            Console.Read();
+
+        }
+    }
+}
