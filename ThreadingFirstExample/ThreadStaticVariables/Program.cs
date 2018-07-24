fd4555afc2f3b446b816f2ca620dad7222e62eb1
Markus Dieckmann <markus.dieckmann@edataprocessing.de>
2018-07-24 13:51:00 +0200

    Thread Static

---
+++ ThreadingFirstExample/ThreadStaticVariables/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ThreadStaticVariables
+{
+    class Program
+    {
+        [ThreadStatic]
+        public static int _field;
+        static void Main(string[] args)
+        {
+
+            new Thread(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    _field++;
+                    Console.WriteLine("Thread A: {0}", _field);
+                    Thread.Sleep(1);
+                }
+            }).Start();
+            new Thread(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    _field++;
+                    Console.WriteLine("Thread B: {0}", _field);
+                    Thread.Sleep(1);
+                }
+            }).Start();
+            Console.ReadKey();
+        }
+    }
+}
