b31727630db01d08f41a84c2e5b3bcbd9af7f741
Markus Dieckmann <markus.dieckmann@edataprocessing.de>
2018-07-24 13:50:51 +0200

    Thread Local

---
+++ ThreadingFirstExample/ThreadLocalVariables/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ThreadLocalVariables
+{
+    class Program
+    {
+        public static ThreadLocal<int> _field = new ThreadLocal<int>(() =>
+        {
+            return Thread.CurrentThread.ManagedThreadId;
+        });
+        static void Main(string[] args)
+        {
+            new Thread(() =>
+            {
+                for (int i = 0; i < _field.Value; i++)
+                {
+                    Console.WriteLine("Thread A: {0}", i);
+                    Thread.Sleep(1);
+                }
+            }).Start();
+            new Thread(() =>
+            {
+                for (int i = 0; i < _field.Value; i++)
+                {
+                    Console.WriteLine("Thread B: {0}", i);
+                    Thread.Sleep(1);
+                }
+            }).Start();
+            Console.ReadKey();
+        }
+    }
+}
