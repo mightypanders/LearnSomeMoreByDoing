d6f540f15a1f6f3e2f55a1f09c4bbaa9cf1d8ef0
Markus Dieckmann <markus.dieckmann@edataprocessing.de>
2018-07-24 13:51:07 +0200

    Thread Pool

---
+++ ThreadingFirstExample/ThreadPool/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ThreadPool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                System.Threading.ThreadPool.QueueUserWorkItem((s) =>
+                {
+                    Console.WriteLine("Working in Thread: {0}", Thread.CurrentThread.ManagedThreadId);
+                });
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
