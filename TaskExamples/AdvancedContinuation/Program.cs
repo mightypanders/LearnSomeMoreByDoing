5a237b4a6cd4aaca234b335b84b2adca0c67b3d1
Markus Dieckmann <markus.dieckmann@edataprocessing.de>
2018-07-24 15:22:55 +0200

    Continue With Advanced

---
+++ TaskExamples/AdvancedContinuation/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedContinuation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<int> t = Task.Run(() =>
+            {
+                return 42;
+            });
+
+            var faultedTask = t.ContinueWith((i) =>
+            {
+                Console.WriteLine("Faulted");
+            }, TaskContinuationOptions.OnlyOnFaulted);
+            var canceledTask = t.ContinueWith((i) =>
+            {
+                Console.WriteLine("Canceled");
+            }, TaskContinuationOptions.OnlyOnCanceled);
+            var completedTask = t.ContinueWith((i) =>
+            {
+                Console.WriteLine("Completed");
+            }, TaskContinuationOptions.OnlyOnRanToCompletion);
+            List<Task> tasks = new List<Task>() { faultedTask, canceledTask, completedTask };
+            Task.WaitAny(tasks.ToArray());
+            Console.Read();
+        }
+    }
+}
