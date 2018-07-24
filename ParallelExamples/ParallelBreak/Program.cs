b599b272dd19008ffc2d399afd127046320ac873
Markus Dieckmann <markus.dieckmann@edataprocessing.de>
2018-07-24 15:54:27 +0200

    Parallel

---
+++ ParallelExamples/ParallelBreak/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelBreak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting");
+            ParallelLoopResult res = Parallel.For(0, 1000, (int i, ParallelLoopState state) =>
+            {
+                Console.WriteLine(i);
+                if (i >= 500)
+                {
+                    Console.WriteLine("Breaking Loop");
+                    state.Break();
+                }
+                return;
+            });
+            Console.WriteLine("loop broken at {0}", res.LowestBreakIteration);
+            Console.Read();
+        }
+    }
+}
---
+++ ParallelExamples/ParallelExamples/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch s = new Stopwatch();
+            s.Start();
+            Parallel.For(0, 10, new ParallelOptions() { MaxDegreeOfParallelism = 10 }, i =>
+              {
+                  Thread.Sleep(1000);
+                  Console.WriteLine("Sleeping");
+              });
+            Console.WriteLine("Elapsed: {0}MS", s.ElapsedMilliseconds);
+            s.Restart();
+            var num = Enumerable.Range(0, 10);
+            Parallel.ForEach(num, i =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("ForEachSleeping");
+            });
+            Console.WriteLine("Elapsed: {0}MS", s.ElapsedMilliseconds);
+            s.Stop();
+            Console.Read();
+        }
+    }
+}
