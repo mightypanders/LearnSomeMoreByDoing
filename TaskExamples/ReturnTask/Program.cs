1620914192be6d032bcbbefb607355b42f2f157e
Markus Dieckmann <markus.dieckmann@edataprocessing.de>
2018-07-24 15:23:21 +0200

    Return from Task

---
+++ TaskExamples/ReturnTask/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReturnTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<int> t = Task.Run(() =>
+            {
+                Thread.Sleep(5000);
+                return 42;
+            });
+            Console.WriteLine(t.Result);
+            Console.Read();
+        }
+    }
+}
