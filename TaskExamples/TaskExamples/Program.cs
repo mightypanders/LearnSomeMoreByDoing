f80efc1010311e55c7ef911c64196684e0792f82
Markus Dieckmann <markus.dieckmann@edataprocessing.de>
2018-07-24 15:23:32 +0200

    Task Example 1

---
+++ TaskExamples/TaskExamples/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task t = Task.Run(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    Console.Write('*');
+                    Thread.Sleep(10);
+                }
+
+            });
+            t.Wait();
+            Console.ReadKey();
+        }
+    }
+}
