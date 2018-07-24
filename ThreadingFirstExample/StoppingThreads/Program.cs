8c245ae9a3728d618b858ad55057408585df4b61
Markus Dieckmann <markus.dieckmann@edataprocessing.de>
2018-07-24 13:50:43 +0200

    Stopping Threads

---
+++ ThreadingFirstExample/StoppingThreads/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StoppingThreads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool stopped = false;
+            Thread t = new Thread(new ThreadStart(() =>
+                {
+                    while (!stopped)
+                    {
+                        Console.WriteLine("Running...");
+                        Thread.Sleep(500);
+                    }
+                }
+                ));
+            t.Start();
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+            stopped = true;
+            t.Join();
+        }
+    }
+}
