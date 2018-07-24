e5c8197eddcb0fc9ad9e489ecc42247fe0b429ca
Markus Dieckmann <markus.dieckmann@edataprocessing.de>
2018-07-24 13:14:11 +0200

    First Example Threading

---
+++ ThreadingFirstExample/ThreadingFirstExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadingFirstExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(new ThreadStart(ThreadMethod));
+            t.Start();
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Main Thread Working");
+                Thread.Sleep(1);
+            }
+            t.Join();
+            Console.Read();
+        }
+        public static void ThreadMethod()
+        {
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+                Thread.Sleep(1);
+            }
+        }
+    }
+}
