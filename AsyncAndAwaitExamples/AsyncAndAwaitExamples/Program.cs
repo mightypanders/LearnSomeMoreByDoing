a8cc74f901a52618923a21df0246b671bb31d3fe
Markus Dieckmann <markus.dieckmann@edataprocessing.de>
2018-07-25 08:36:27 +0200

    Async 1

---
+++ AsyncAndAwaitExamples/AsyncAndAwaitExamples/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAndAwaitExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+            Console.ReadKey();
+            var result = DownloadContent().Result;
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+        public static async Task<string> DownloadContent()
+        {
+            using (HttpClient c = new HttpClient())
+            {
+                var res = await c.GetStringAsync("http://www.microsoft.com");
+                return res;
+            }
+        }
+    }
+}
