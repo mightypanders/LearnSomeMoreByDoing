ad5c0554ec440340e1e81716647af4b701f8c305
Markus Dieckmann <markus.dieckmann@edataprocessing.de>
2018-07-25 12:38:20 +0200

    blocking collection

---
+++ ConcurrentCollections/ConcurrentCollections/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcurrentCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BlockingCollection<string> col = new BlockingCollection<string>();
+            Task read = Task.Run(() =>
+            {
+                foreach (var item in col.GetConsumingEnumerable())
+                    Console.WriteLine(item);
+            });
+            Task write = Task.Run(() =>
+            {
+                while (true)
+                {
+                    string s = Console.ReadLine();
+                    if (string.IsNullOrWhiteSpace(s))
+                    {
+                        col.CompleteAdding();
+                        break;
+
+                    }
+                    col.Add(s);
+                }
+            });
+            write.Wait();
+            Task secondWrite = Task.Run(() =>
+            {
+                while (true)
+                {
+                    string s = Console.ReadLine();
+                    if (string.IsNullOrWhiteSpace(s))
+                        break;
+                    col.Add(s);
+                }
+            });
+            secondWrite.Wait();
+        }
+    }
+}
