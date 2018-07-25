6d0f3aed5604dc19297b2fc45c0543b6a4179c8d
Markus Dieckmann <markus.dieckmann@edataprocessing.de>
2018-07-25 11:19:29 +0200

    plinq for all

---
+++ PLINQExamples/ForAll/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = Enumerable.Range(0, 100);
+            var res = num.AsParallel().AsOrdered().Where(i => i % 2 == 0);
+            res.ForAll(e => Console.WriteLine(e));
+        }
+    }
+}
