b343e04f1a7996dfa1fcd01fa643cd6cb7db89df
Markus Dieckmann <markus.dieckmann@edataprocessing.de>
2018-07-25 11:19:35 +0200

    plinq

---
+++ PLINQExamples/PLINQExamples/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PLINQExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+            var num = Enumerable.Range(0, 100000000);
+            var parRes = num
+                .AsParallel()
+                .AsOrdered()
+                .Where(i => i % 2 == 0)
+                .ToArray();
+            Console.WriteLine("output?");
+            Console.ReadKey();
+            foreach (var item in parRes)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
