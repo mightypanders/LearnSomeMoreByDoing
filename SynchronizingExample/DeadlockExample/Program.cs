84833f6a5d4475ffb69b9b63f2e9135648921e16
Markus Dieckmann <markus.dieckmann@edataprocessing.de>
2018-07-25 13:34:12 +0200

    non working deadlock example

---
+++ SynchronizingExample/DeadlockExample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DeadlockExample
+{
+    //this doesn't really work 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object lockA = new object(), lockB = new object();
+            var up = Task.Run(() =>
+            {
+                lock (lockA)
+                {
+                    lock (lockB)
+                    {
+                        Console.WriteLine("Lock A B");
+                    }
+                }
+            });
+            lock (lockB)
+            {
+                lock (lockA)
+                {
+                    Console.WriteLine("Lock B A");
+                    Console.ReadKey();
+                }
+            }
+            up.Wait();
+            Console.ReadKey();
+        }
+    }
+}
