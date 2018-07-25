681a7847512ecced639196b86b5f0ca6927df003
Markus Dieckmann <markus.dieckmann@edataprocessing.de>
2018-07-25 09:31:48 +0200

    scaling

--- AsyncAndAwaitExamples/ScalingAsync/Program.cs
+++ AsyncAndAwaitExamples/ScalingAsync/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ScalingAsync
@@ -10,6 +11,20 @@ namespace ScalingAsync
     {
         static void Main(string[] args)
         {
+        }
+
+        static Task SleepA(int mil)
+        {
+            return Task.Run(() => Thread.Sleep(mil));
+        }
+
+        static Task SleepB(int mil)
+        {
+            TaskCompletionSource<bool> tcs = null;
+            var t = new Timer(delegate { tcs.TrySetResult(true); }, null, -1, -1);
+            tcs = new TaskCompletionSource<bool>(t);
+            t.Change(mil, -1);
+            return tcs.Task;
         }
     }
 }
