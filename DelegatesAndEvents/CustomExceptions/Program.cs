ced20b4c16ec34df547227b922bf79cd6123c474
mightypanders <mighty.panders@gmail.com>
2018-07-26 15:05:31 +0200

    custom exceptions

---
+++ DelegatesAndEvents/CustomExceptions/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class OrderProcessingExcpetion : Exception, ISerializable
+    {
+        public OrderProcessingExcpetion(int orderID)
+        {
+            OrderId = orderID;
+            this.HelpLink = "giyf";
+        }
+        public OrderProcessingExcpetion(int orderID, string message) : base(message)
+        {
+            OrderId = orderID;
+            this.HelpLink = "giyf";
+        }
+        public OrderProcessingExcpetion(int orderID, string message, Exception ex) : base(message, ex)
+        {
+            OrderId = orderID;
+            this.HelpLink = "giyf";
+        }
+
+        public int OrderId { get; private set; }
+    }
+}
