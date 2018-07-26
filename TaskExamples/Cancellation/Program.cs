b494cbd666a2de6f402a188bb9d2548f68e86048
mightypanders <mighty.panders@gmail.com>
2018-07-26 15:06:37 +0200

    task: cancellation

---
+++ TaskExamples/Cancellation/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cancellation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+            CancellationToken token = cts.Token;
+            Task t = Task.Run(() =>
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    Console.WriteLine("*");
+                    Thread.Sleep(1000);
+                }
+                token.ThrowIfCancellationRequested();
+            }, token);
+            try
+            {
+                Console.WriteLine(" Enter to stop");
+                Console.ReadLine();
+                cts.Cancel();
+                t.Wait();
+            }
+            catch (AggregateException e)
+            {
+                Console.WriteLine(e.InnerExceptions[0].Message);
+            }
+            Console.WriteLine("Enter to end");
+            Console.ReadLine();
+        }
+    }
+}
