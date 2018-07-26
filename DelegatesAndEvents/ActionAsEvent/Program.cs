d425491cc1543174ed5845a68ecc03f975181a91
mightypanders <mighty.panders@gmail.com>
2018-07-26 15:05:22 +0200

    actions as events

---
+++ DelegatesAndEvents/ActionAsEvent/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActionAsEvent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pub p = new Pub();
+            p.OnChange += () => Console.WriteLine("Method 1");
+            p.OnChange += () => Console.WriteLine("Method 2");
+            p.Raise();
+            Console.ReadLine();
+
+        }
+    }
+    public class Pub
+    {
+        public Action OnChange { get; set; }
+        public void Raise()
+        {
+            OnChange?.Invoke();
+        }
+    }
+}
