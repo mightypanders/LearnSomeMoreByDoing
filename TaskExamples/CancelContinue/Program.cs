ed4f90f05ac6483ad7a56f2e70b1c92e9065f873
mightypanders <mighty.panders@gmail.com>
2018-07-26 15:06:31 +0200

    task: cancel continue

---
+++ TaskExamples/CancelContinue/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CancelContinue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+            CancellationToken token = cts.Token;
+            Task task = Task.Run(() =>
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    Console.WriteLine("*");
+                    Thread.Sleep(1000);
+                }
+                throw new OperationCanceledException();
+            }, token).ContinueWith((t) =>
+            {
+                t.Exception.Handle((e) => true);
+                Console.WriteLine("You have canceled the task. You Monster");
+            }, TaskContinuationOptions.OnlyOnCanceled);
+            try
+            {
+                Console.WriteLine(" Enter to stop");
+                Console.ReadLine();
+                cts.Cancel();
+                task.Wait();
+            }
+            catch (AggregateException e)
+            {
+                Console.WriteLine(e.InnerExceptions[0].Message);
+            }
+            Console.WriteLine("Enter to end");
+            Console.ReadLine();
+        }
+    }
+}
