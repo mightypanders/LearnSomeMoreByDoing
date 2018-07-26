3e2717233144c73a9f7b4256c1128b9be700ffed
mightypanders <mighty.panders@gmail.com>
2018-07-26 15:05:52 +0200

    multicast delegates

---
+++ DelegatesAndEvents/MulticastDelelgates/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MulticastDelelgates
+{
+    class Program
+    {
+        public static void MethodOne()
+        {
+            Console.WriteLine("Method one");
+        }
+        public static void MethodTwo()
+        {
+            Console.WriteLine("Method Two");
+        }
+        public delegate void Del();
+        static void Main(string[] args)
+        {
+            Del d = MethodOne;
+            d += MethodTwo;
+            d();
+            Console.ReadLine();
+        }
+    }
+}
